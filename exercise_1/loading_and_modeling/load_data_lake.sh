ebbf84326a3ac4d396e2b0a81dcf6ba74fb6bfc9
younusa <younusahmed23@gmail.com>
2017-02-16 19:00:15 -0500

    revised shell script

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -13,24 +13,26 @@ cd ~staging/exercise_1
 wget "https://data.medicare.gov/views/bg9k-emty/files/6c902f45-e28b-42f5-9f96-ae9d1e583472?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip" -O medicaredata.zip
 unzip medicaredata.zip
 
-#Creating HDFS Directory
+#Creating HDFS Directories
 hdfs dfs -mkdir /user/w205/hospital_compare
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir /user/w205/hospital_compare/Measures
+hdfs dfs -mkdir /user/w205/hospital_compare/surveys_responses
 
 #remove header from each csv file and rename file. Move file to HDFS directory
 tail -n +2 "Hospital General Information.csv" > hospitals.csv
-hdfs dfs -put hospitals.csv /user/w205/hospital_compare
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare/hospitals
 tail -n +2 "Timely and Effective Care - Hospital.csv" > effective_care.csv
-hdfs dfs -put effective_care.csv /user/w205/hospital_compare
+hdfs dfs -put effective_care.csv /user/w205/hospital_compare/effective_care
 tail -n +2 "Readmissions and Deaths - Hospital.csv" > readmissions.csv
-hdfs dfs -put readmissions.csv /user/w205/hospital_compare
+hdfs dfs -put readmissions.csv /user/w205/hospital_compare/readmissions
 tail -n +2 "Measure Dates.csv" > Measures.csv
-hdfs dfs -put Measures.csv /user/w205/hospital_compare
+hdfs dfs -put Measures.csv /user/w205/hospital_compare/Measures
 tail -n +2 "hvbp_hcahps_11_10_2016.csv" > surveys_responses.csv
-hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare
+hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare/surveys_responses
 
 
 #Change back to orig directory
 cd "$MY_DIR"
-
-
-
