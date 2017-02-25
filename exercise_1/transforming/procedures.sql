867f1a2d334f0d0dfe1555a3191d93c893af87fc
younusa <younusahmed23@gmail.com>
2017-02-24 22:05:08 -0500

    sql edits

--- exercise_1/transforming/procedures.sql
+++ exercise_1/transforming/procedures.sql
@@ -3,9 +3,13 @@ DROP TABLE procedures;
 Create Table procedures as
 SELECT	
 Provider_ID,
-State,
 Condition,
 Measure_ID,
 Measure_Name,
 Score
 FROM effective_care;
+
+ALTER TABLE procedures
+CHANGE Score Score int
+;
+
--- exercise_1/transforming/readmissions.sql
+++ exercise_1/transforming/readmissions.sql
@@ -3,7 +3,6 @@ DROP TABLE readmissionsrate;
 Create Table readmissionsrate as
 SELECT	
 Provider_ID,
-State,
 Measure_ID,
 Measure_Name,
 Score,
