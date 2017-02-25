443169a677423cc09cc706b07d61d829f192b969
younusa <younusahmed23@gmail.com>
2017-02-24 21:46:01 -0500

    sql edits

--- exercise_1/transforming/my_hospitals.sql
+++ exercise_1/transforming/my_hospitals.sql
@@ -13,7 +13,5 @@ Phone_Number,
 Hospital_Overall_Rating
 FROM hospitals;
 
-ALTER TABLE my_hospitals
-CHANGE Hospital_Overall_Rating Hospital_Overall_Rating int
-CHANGE provider_id provider_id int
-;
+ALTER TABLE my_hospitals CHANGE Hospital_Overall_Rating Hospital_Overall_Rating int;
+ALTER TABLE my_hospitals CHANGE provider_id provider_id int;
